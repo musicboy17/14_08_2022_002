9f00a9738ace6193e19671b77faefa063a4614af
musicboy17 <pratham@yandex.ru>
2022-08-14 21:21:03 +0300

    урок N3_0052

--- Lection_03_005/Program.cs
+++ Lection_03_005/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// И ещё раз продолжение урока
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+        while (i < count)
+        {
+            result = result + text;
+            i++;
+        }
+        return result;
+}
+string res = Method4(10, "asdf");
+
+Console.WriteLine(res);
+
+
